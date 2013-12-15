d13a53dd7b7e718089a768036d8a5c68fae0bb8f
Ross Davies <steelbadger@gmail.com>
2013-12-15 16:07:51 +0000

    Reduce render target bit depth

--- DirectXProject/DirectXProject/RenderTarget.cpp
+++ DirectXProject/DirectXProject/RenderTarget.cpp
@@ -29,7 +29,7 @@ bool RenderTarget::Initialize(ID3D11Device* device, int textureWidth, int textur
 	textureDesc.Height = textureHeight;
 	textureDesc.MipLevels = 1;
 	textureDesc.ArraySize = 1;
-	textureDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	textureDesc.Format = DXGI_FORMAT_R16G16B16A16_FLOAT;
 	textureDesc.SampleDesc.Count = 1;
 	textureDesc.Usage = D3D11_USAGE_DEFAULT;
 	textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
